6b67f5b032b54aa23945ac22a646e2cc29e4cca8
Sergio Fenoll <sergio@fenoll.be>
2024-04-09 13:33:56 +0200

    feat: add migration code

---
+++ constants.js
@@ -0,0 +1,19 @@
+export default {
+  GRAPHS: {
+    KANSELARIJ: 'http://mu.semte.ch/graphs/organizations/kanselarij',
+    PUBLIC: 'http://mu.semte.ch/graphs/public',
+  },
+  AGENDA_ITEM_TYPES: {
+    NOTA: 'http://themis.vlaanderen.be/id/concept/agendapunt-type/dd47a8f8-3ad2-4d5a-8318-66fc02fe80fd',
+    MEDEDELING: 'http://themis.vlaanderen.be/id/concept/agendapunt-type/8f8adcf0-58ef-4edc-9e36-0c9095fd76b0',
+  },
+  SUBCASE_TYPES: {
+    BEKRACHTIGING: 'http://themis.vlaanderen.be/id/concept/procedurestap-type/bdba2bbc-7af6-490b-98a8-433955cfe869',
+  },
+  MEETING_TYPES: {
+    PVV: 'http://themis.vlaanderen.be/id/concept/vergaderactiviteit-type/9b4701f8-a136-4009-94c6-d64fdc96b9a2',
+  },
+  PIECE_TYPES: {
+    DECREET: 'https://data.vlaanderen.be/id/concept/AardWetgeving/Decreet',
+  },
+}
